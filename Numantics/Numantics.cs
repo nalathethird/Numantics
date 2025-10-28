63df93c6c926b4bae6181213a92993d910d75e6c
NalaTheThird <36301692+nalathethird@users.noreply.github.com>
2025-10-27 19:04:16 -0500

    Updated to include InverseTrig (Removed the Fun -See fun-included branch)

--- Numantics/Numantics.cs
+++ Numantics/Numantics.cs
@@ -12,12 +12,12 @@ using ResoniteModLoader;
 
 namespace Numantics {
 	public class Numantics : ResoniteMod {
-		internal const string VERSION_CONSTANT = "1.0.1";
+		internal const string VERSION_CONSTANT = "1.0.2";
 		public override string Name => "Numantics";
 		public override string Author => "NalaTheThird";
 		public override string Version => VERSION_CONSTANT;
 		public override string Link => "https://github.com/nalathethird/Numantics";
-		
+
 		[AutoRegisterConfigKey]
 		private static readonly ModConfigurationKey<bool> EnableValueFieldMath =
 			new ModConfigurationKey<bool>("enable_math", "Enable math processing in input fields - Disables/Enables Entire Mod", () => true);
@@ -31,10 +31,6 @@ namespace Numantics {
 			new ModConfigurationKey<bool>("round_results", "Round all calculated results to nearest integer (e.g., 5*0.5=3 instead of 2.5)", () => false);
 
 		[AutoRegisterConfigKey]
-		private static readonly ModConfigurationKey<bool> EnableEasterEggs =
-			new ModConfigurationKey<bool>("enable_easter_eggs", "Enable 'Easter eggs' - (Who knows what you might get with this on...)", () => false);
-
-		[AutoRegisterConfigKey]
 		private static readonly ModConfigurationKey<bool> VerboseLogging =
 			new ModConfigurationKey<bool>("verbose_logging", "Enables Verbose Logging (Not Likely needed for regular folks)", () => false);
 
@@ -46,8 +42,6 @@ namespace Numantics {
 
 			var harmony = new Harmony("com.nalathethird.Numantics");
 			harmony.PatchAll();
-			Msg("NalaTheThird was here :p - Thanks for using my mod, please consider staring the Repo (the link value in the mod settings), or sending me a Donation on Ko-Fi: https://www.ko-fi.com/zeianala | It really motivates me to make more things and keep my mods maintained!");
-			Msg("Harmony patch applied! Ready to Crunch the Numbers with Numantics!");
 		}
 
 		[HarmonyPatch(typeof(TextEditor))]
@@ -83,9 +77,9 @@ namespace Numantics {
 
 					Type fieldType = GetFieldType(__instance, verbose);
 					bool isStringField = fieldType == typeof(string);
-					
+
 					if (verbose) Msg($"Detected field type: {fieldType?.Name ?? "Unknown"}");
-					
+
 					if (isStringField) {
 						if (!Config.GetValue(IncludeStrings)) {
 							if (verbose) Msg("Editing string field but include_strings is disabled, skipping");
@@ -115,48 +109,6 @@ namespace Numantics {
 						Msg($"Replaced operators: '{text}' => '{exprText}'");
 					}
 
-					// EE Values and Checks
-					bool easterEggs = Config.GetValue(EnableEasterEggs);
-					if (easterEggs) {
-
-						if (originalText.ToLower().Contains("sqrt")) {
-							Msg("EASTER EGG - Squirt!");
-						}
-
-						if (originalText == "0/0") {
-							Msg("EASTER EGG - NaN ERROR! 0! VOID, ERROR! ERROR! You Destroy.e.d t..h..e w...o...r..l..d.... (Leaving world...)");
-							__instance.Text.Target.Text = "NaN";
-							__instance.World.RunSynchronously(() => {
-								if (__instance.World.IsAuthority) {
-									Userspace.EndSession(__instance.World);
-								} else {
-									Userspace.LeaveSession(__instance.World);
-								}
-							});
-							return;
-						}
-						if (originalText == "2+2") {
-							__instance.Text.Target.Text = "5";
-							Msg("EASTER EGG - Look, we get it, your '2s' are 'very large'. Just take the 5 and go.");
-							return;
-						}
-						if (originalText == "6/2*(1+2)") {
-							__instance.Text.Target.Text = "9";
-							Msg("EASTER EGG - It's 9. Don't you DARE start this again. We know who you are.");
-							return;
-						}
-						if (originalText == "69+420") {
-							__instance.Text.Target.Text = "0";
-							Msg("EASTER EGG - No mating or smoking :< (at least the math checks out) Still going to make this 0 though for your rampant behavior.");
-							return;
-						}
-						if (originalText == "9+10") {
-							__instance.Text.Target.Text = "21";
-							Msg("EASTER EGG - U Stoopid, ITS 19!)");
-							return;
-						}
-					}
-
 					// Expression Evaluator - If you cant express it, you cant impress it! | Main Logic for Math Evaluation
 					if (TryEvaluateExpression(exprText, out string result, verbose)) {
 						if (Config.GetValue(RoundResults)) {
@@ -169,7 +121,7 @@ namespace Numantics {
 
 						Msg($"SUCCESS - Evaluated '{originalText}' => '{result}'");
 						__instance.Text.Target.Text = result;
-						
+
 						if (verbose) Msg($"Updated Text.Target.Text to '{result}'");
 					} else {
 						if (double.TryParse(exprText, NumberStyles.Float, CultureInfo.InvariantCulture, out double numValue)) {
@@ -180,7 +132,7 @@ namespace Numantics {
 								if (verbose) Msg($"Rounded {numValue} to {rounded}");
 								numResult = rounded.ToString(CultureInfo.InvariantCulture);
 							}
-							
+
 							Msg($"SUCCESS - Evaluated '{originalText}' => '{numResult}'");
 							__instance.Text.Target.Text = numResult;
 							if (verbose) Msg($"Updated Text.Target.Text to '{numResult}'");
@@ -278,18 +230,21 @@ namespace Numantics {
 
 			private static string ProcessMathFunctions(string input, bool verbose) {
 				string processed = input;
-				
+
 				processed = ProcessFunction(processed, "sqrt", Math.Sqrt, verbose);
 				processed = ProcessFunction(processed, "sin", x => Math.Sin(x * Math.PI / 180.0), verbose);
 				processed = ProcessFunction(processed, "cos", x => Math.Cos(x * Math.PI / 180.0), verbose);
 				processed = ProcessFunction(processed, "tan", x => Math.Tan(x * Math.PI / 180.0), verbose);
+				processed = ProcessFunction(processed, "asin", x => Math.Asin(x) * 180.0 / Math.PI, verbose);
+				processed = ProcessFunction(processed, "acos", x => Math.Acos(x) * 180.0 / Math.PI, verbose);
+				processed = ProcessFunction(processed, "atan", x => Math.Atan(x) * 180.0 / Math.PI, verbose);
 				processed = ProcessFunction(processed, "log10", Math.Log10, verbose);
 				processed = ProcessFunction(processed, "log", Math.Log, verbose);
 				processed = ProcessFunction(processed, "ln", Math.Log, verbose);
 				processed = ProcessFunction(processed, "abs", Math.Abs, verbose);
 				processed = ProcessFunction(processed, "floor", Math.Floor, verbose);
 				processed = ProcessFunction(processed, "ceil", Math.Ceiling, verbose);
-				
+
 				return processed;
 			}
 
@@ -298,21 +253,21 @@ namespace Numantics {
 				while (regex.IsMatch(input)) {
 					var match = regex.Match(input);
 					string innerExpr = match.Groups[1].Value;
-					
+
 					double innerValue;
 					if (double.TryParse(innerExpr, NumberStyles.Float, CultureInfo.InvariantCulture, out innerValue)) {
 					} else {
 						var innerTable = new DataTable();
 						innerValue = Convert.ToDouble(innerTable.Compute(innerExpr, ""));
 					}
-					
+
 					double funcResult = func(innerValue);
-					
+
 					// Floating Point Rounding - We all float down here...
 					funcResult = Math.Round(funcResult, 10, MidpointRounding.AwayFromZero);
-					
+
 					input = input.Replace(match.Value, funcResult.ToString(CultureInfo.InvariantCulture));
-					
+
 					if (verbose) Msg($"Evaluated {funcName}({innerExpr}) = {funcResult}");
 				}
 				return input;
@@ -322,15 +277,15 @@ namespace Numantics {
 				result = input;
 
 				// Container Checks - Operator, do we have a dial tone? - (basically checks for math operators then allows calculations)
-				if (!(input.Contains("+") || input.Contains("-") || input.Contains("*") || 
-				      input.Contains("/") || input.Contains("^") || input.Contains("%"))) {
+				if (!(input.Contains("+") || input.Contains("-") || input.Contains("*") ||
+					  input.Contains("/") || input.Contains("^") || input.Contains("%"))) {
 					if (verbose) Msg("No math operators found in input");
 					return false;
 				}
 
 				try {
 					if (verbose) Msg($"Attempting to parse expression: '{input}'");
-					
+
 					string processedInput = input;
 
 					// Percentage Handler - you just HAD to use these instead of decimals didn't you? For SHAME!
@@ -375,16 +330,16 @@ namespace Numantics {
 
 						if (verbose) Msg($"Evaluated power: {left}^{right} = {powResult}");
 					}
-					
+
 					var table = new DataTable();
 					var evaluated = table.Compute(processedInput, "");
 					double resultValue = Convert.ToDouble(evaluated);
 
 					// Floating Point Precision Handling
 					resultValue = Math.Round(resultValue, 7, MidpointRounding.AwayFromZero);
-					
+
 					if (verbose) Msg($"Evaluated to: {resultValue}");
-					
+
 					result = resultValue.ToString(CultureInfo.InvariantCulture);
 					return true;
 				} catch (Exception ex) {
@@ -395,8 +350,8 @@ namespace Numantics {
 			}
 
 			private static int FindNumberStart(string expr, int from) {
-				while (from > 0 && (char.IsDigit(expr[from]) || expr[from] == '.' || 
-				    (expr[from] == '-' && from > 0 && !char.IsDigit(expr[from - 1])))) {
+				while (from > 0 && (char.IsDigit(expr[from]) || expr[from] == '.' ||
+					(expr[from] == '-' && from > 0 && !char.IsDigit(expr[from - 1])))) {
 					from--;
 				}
 				if (from == 0 && (char.IsDigit(expr[0]) || expr[0] == '-' || expr[0] == '.')) {
@@ -404,10 +359,10 @@ namespace Numantics {
 				}
 				return from + 1;
 			}
-			
+
 			private static int FindNumberEnd(string expr, int from) {
-				while (from < expr.Length && (char.IsDigit(expr[from]) || expr[from] == '.' || 
-				    (expr[from] == '-' && from == 0))) {
+				while (from < expr.Length && (char.IsDigit(expr[from]) || expr[from] == '.' ||
+					(expr[from] == '-' && from == 0))) {
 					from++;
 				}
 				return from;
